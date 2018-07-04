d38cec323cdd762c60475c6f8592fe0d1c178272
Si2k63 <poc_is_good@hotmail.com>
2018-07-04 14:14:31 +0100

    Added getters and setters for card class, including prime number based unique identifiers for each card. Added draw and contains methods to deck class. Added static methods for validating card ranks and suits to deck class.

--- card.php
+++ card.php
@@ -5,22 +5,62 @@ class card
     private $rank;
     private $suit;
 
-    function __construct($rank, $suit)
+    private static $rankPrimes = array(
+        '2' => 2,
+        '3' => 3,
+        '4' => 5,
+        '5' => 7,
+        '6' => 9,
+        '7' => 11,
+        '8' => 13,
+        '9' => 17,
+        'T' => 19,
+        'J' => 23,
+        'Q' => 29,
+        'K' => 31,
+        'A' => 37,
+    );
+
+    private static $suitPrimes = array(
+        'c' => 41,
+        'h' => 43,
+        'd' => 47,
+        's' => 53
+    );
+
+    public function __construct($rank, $suit)
     {
-        $this->rank = $rank;
-        $this->suit = $suit;
+        if (deck::isValidRank($rank) && deck::isValidSuit($suit))
+        {
+            $this->rank = $rank;
+            $this->suit = $suit;
+        }
+        else
+        {
+            return false;
+        }
     }
 
-    function getRank()
+    public function getId()
+    {
+        return $rankPrimes[$this->rank] * $suitPrimes[$suit];
+    }
+
+    public function getRank()
     {
         return $this->rank;
     }
 
-    function getSuit()
+    public function getSuit()
     {
         return $this->suit;
     }
-}
 
+
+    public function display()
+    {
+        echo $this->rank . $this->suit . PHP_EOL;
+    }
+}
 
 ?>
--- deck.php
+++ deck.php
@@ -2,17 +2,18 @@
 
 class deck
 {
-    var $suits = array('c','h','d','s');
-    var $ranks = array('2','3','4','5','6','7','8','9','10','J','Q','K','A');
-    var $cards = array();
+    private static $suits = array('c','h','d','s');
+    private static $ranks = array('2','3','4','5','6','7','8','9','T','J','Q','K','A');
+    private $cards = array();
 
     public function __construct()
     {
-        for ($i = 0; $i < count($this->suits); $i++)
+        for ($i = 0; $i < count(self::$suits); $i++)
         {
-            for ($k = 0; $k < count($this->ranks); $k++)
+            for ($k = 0; $k < count(self::$ranks); $k++)
             {
-                $card = new card($this->ranks[$k], $this->suits[$i]);
+                $card = new card(self::$ranks[$k], self::$suits[$i]);
+                $card->display();
                 $this->cards[]=$card;
             }
         }
@@ -20,8 +21,41 @@ class deck
 
     public function shuffle()
     {
-        shuffle($this->cards);
+        if (count($this->cards))
+        {
+            shuffle($this->cards);
+        }
+        else
+        {
+            return false;
+        }
     }
+
+    public function draw()
+    {
+        if (count($this->cards))
+        {
+            $card = $this->cards[0];
+            array_splice($this->cards, 0, 1);
+            return $card;
+        }
+    }
+
+    public function contains($card)
+    {
+        return in_array($card, $this->cards);
+    }
+
+    public static function isValidSuit($suit)
+    {
+        return in_array($suit, self::$suits);
+    }
+
+    public static function isValidRank($rank)
+    {
+        return in_array($rank, self::$ranks);
+    }
+
 }
 
 ?>
